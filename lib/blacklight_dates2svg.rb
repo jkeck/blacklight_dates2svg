b957d6887601df51b3e528366c0306e9aaf57e84
Jessie Keck <jessie.keck@gmail.com>
2013-04-12 11:34:46 -0700

    Add before_filter when we're included into a controller to inject fq parameter into the solr query.

--- lib/blacklight_dates2svg.rb
+++ lib/blacklight_dates2svg.rb
@@ -1,12 +1,27 @@
 require "blacklight_dates2svg/engine"
 require 'dates2svg'
+require 'date_range_solr_query'
 
 module BlacklightDates2SVG
   extend ActiveSupport::Concern
   included do
+    self.solr_search_params_logic += [:add_dates2svg_range_query]
     helper_method :render_date_range_selector_grid, :render_date_range_value, :date_range_form_omit_keys
   end
   
+  def add_dates2svg_range_query(solr_params, user_params)
+    if user_params["dates"] and !user_params["date-start"].blank?
+      range_query = DateRangeSolrQuery.new(:start_date => user_params['date-start'],
+                                           :end_date => user_params['date-end'],
+                                           :field => blacklight_config.search_date_field).range_query
+      if solr_params[:fq]
+        solr_params[:fq] << range_query
+      else
+        solr_params[:fq] = [range_query]
+      end
+    end
+  end
+
   def render_date_range_selector_grid(options)
     response ||= (options[:response] || @response)
     if @response
